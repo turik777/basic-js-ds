4762aab8945681e736c79d8e9a64d295da4db837
turik777 <52492191+turik777@users.noreply.github.com>
2025-01-06 00:18:51 +0300

    feat: complete remove from list subtask

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let current = l;
+  current.value === k ? l = l.next : null;
+  while (current.next) {
+    current.next.value === k
+      ? current.next = current.next.next
+      : current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
